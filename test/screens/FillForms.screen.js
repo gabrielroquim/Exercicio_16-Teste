7b174cfae19734cf5bff62f1cb8cb0e7dca36790
Gabriel Roquim <gabrielroquim@hotmail.com>
2022-04-04 19:35:44 -0300

    salve

--- test/screens/FillForms.screen.js
+++ test/screens/FillForms.screen.js
@@ -2,15 +2,12 @@ class FillFormsScreen {
 
     get #inputField() { return $('//android.widget.EditText[@content-desc="text-input"]') }
     get #switch() { return $('~switch') }
-    get #dropDown() { return $('~Dropdown') }
-    //get #dropdownMenuText() { return $('~text1')}    
+    get #dropDown() { return $('~Dropdown') } 
     get #dropdownMenuText() { return $('android=new UiSelector().text("webdriver.io is awesome")') }
-    //get #dropdownMenuText() { return $('android=new UiSelector().index(1)') }
     get #clickconfirmaText() { return $('android=new UiSelector().index(1)') }
     get #btnActive() { return $('~button-Active') }
     get #clickDropActive() { return $('android=new UiSelector().text("OK")') }
-    get #okButton(){ return $('android=new UiSelector().index(2)')}
-    //get #clickOk() { return $('//android.widget.Button[3]') }
+    get #okButton(){ return $('android=new UiSelector().index(2)')}    
 
     async typeInputField(input) { await this.#inputField.setValue(input) } // o setValue, envia uma sequencia de elementos do teclado, documentaçã API webdririo  
     async ticSwitch() { await this.#switch.click() }
@@ -24,7 +21,7 @@ class FillFormsScreen {
     async textDropActive() {
         await this.#clickDropActive.waitForExist()
        return await this.#clickDropActive.getText()
-        //return await this.#clickDropActive.click()
+      
     }
     async confirmaOk() {
         await this.#okButton.waitForExist()
